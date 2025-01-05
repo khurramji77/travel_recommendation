4db58a4e2b16cfbe2eddd66e5793a9d698b6a6fd
khurramji77 <khurramjia@hotmail.com>
2025-01-05 05:56:39 -0500

    Implemented Search

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -64,36 +64,30 @@ function generateReport() {
     }
 }
 
-addPatientButton.addEventListener("click", addPatient);
+// addPatientButton.addEventListener("click", addPatient);
 
-    function searchCondition() {
-        const input = document.getElementById('conditionInput').value.toLowerCase();
-        const resultDiv = document.getElementById('result');
-        resultDiv.innerHTML = '';
+function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
 
-        fetch('health_analysis.json')
-          .then(response => response.json())
-          .then(data => {
-            const condition = data.conditions.find(item => item.name.toLowerCase() === input);
-
-            if (condition) {
-              const symptoms = condition.symptoms.join(', ');
-              const prevention = condition.prevention.join(', ');
-              const treatment = condition.treatment;
-
-              resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-              resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-
-              resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-              resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-              resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+    fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            const destinations = data[input.toLowerCase()];
+            if (destinations && destinations.length > 0) {
+                destinations.forEach(destination => {
+                    resultDiv.innerHTML += `<h2>${destination.name}</h2>`;
+                    resultDiv.innerHTML += `<img src="${destination.imageUrl}" alt="hjh">`;
+                    resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${destination.description}</p>`;
+                })
             } else {
-              resultDiv.innerHTML = 'Condition not found.';
+                resultDiv.innerHTML = 'Condition not found.';
             }
-          })
-          .catch(error => {
+        })
+        .catch(error => {
             console.error('Error:', error);
             resultDiv.innerHTML = 'An error occurred while fetching data.';
-          });
-      }
-        btnSearch.addEventListener('click', searchCondition);
+        });
+}
+btnSearch.addEventListener('click', searchCondition);
